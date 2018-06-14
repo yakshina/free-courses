e6930c1338537fc977c031a500d404e942816b7c
yakshina <yakshina_kristina@mail.ru>
2018-06-15 01:31:31 +0300

    task3

---
+++ task3.js
@@ -0,0 +1,18 @@
+var t1=a=process.argv[2];
+var t2=a=process.argv[3];
+
+if (t1+t2>3600){
+    process.stdout.write(Math.floor((t1+t2)/3600) + ' час '+ ((t1+t2)%60)%60 + ' минут '+(t1+t2)%60 + ' секунд');
+}
+else if (((t1+t2)>=3600)&&((t1+t2)%3600==0)){
+    process.stdout.write(Math.floor((t1+t2)/3600) + ' час');
+}
+else if (t1+t2>60){
+    process.stdout.write(Math.floor((t1+t2)/60) + ' минут '+(t1+t2)%60 + ' секунд');
+}
+else if (((t1+t2)>=60)&&((t1+t2)%60==0)){
+    process.stdout.write(Math.floor((t1+t2)/60) + ' минут');
+}
+else if (t1+t2<60){
+    process.stdout.write((t1+t2) + ' секунды');
+}
