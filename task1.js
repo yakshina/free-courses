a5b06dc09023fa485025d779e6dabc4efdceac2d
yakshina <yakshina_kristina@mail.ru>
2018-06-15 23:37:29 +0300

    task1

--- task1.js
+++ task1.js
@@ -1,4 +1,4 @@
 var a=process.argv[2];
-a=a.toLowerCase().replace(/\s/g, '');
+a=a.toLowerCase().replace(/\s/g, '').replace(/[^\w\s]|_/g, "");
 var b=a.split('').reverse().join('');
 process.stdout.write((a===b)?'YES':'NO');
