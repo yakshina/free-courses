e077345bfc77f19a86f7d8108d9b0f43faa294bc
yakshina <yakshina_kristina@mail.ru>
2018-06-16 22:15:54 +0300

    task2

--- task2.js
+++ task2.js
@@ -1,86 +1,89 @@
 var d = process.argv[2];
 var m = process.argv[3];
 var y = process.argv[4];
-var n = String(m);
+var n = '';
+n = n + m;
+var k=0;
 if((y%4==0)&&(y%100!==0)||(y%400==0))
 {switch (n) 
     {
 case 'января':
-process.stdout.write(31-d+335);
+k=31-d+335;
 break;
 case 'февраля':
-process.stdout.write(29-d+306);
+k=29-d+306;
 break;
 case 'марта':
-process.stdout.write(31-d+276);
+k=31-d+276;  
 break;
 case 'апреля':
-process.stdout.write(30-d+246);
+k=30-d+246;
 break;
 case 'мая':
-process.stdout.write(31-d+215);
+k=31-d+215;
 break;
 case 'июня':
-process.stdout.write(30-d+185);
+k=30-d+185;
 break;
 case 'июля':
-process.stdout.write(31-d+154);
+k=31-d+154;
 break;
 case 'августа':
-process.stdout.write(31-d+123);
+k=31-d+123;
 break;
 case 'сентября':
-process.stdout.write(30-d+93);
+k=30-d+93;
 break;
 case 'октября':
-process.stdout.write(31-d+62);
+k=31-d+62;
 break;
 case 'ноября':
-process.stdout.write(30-d+32);
+k=30-d+32;
 break;
 case 'декабря':
-process.stdout.write(31-d);
+k=31-d;
 break;
 } 
 }
 else {
 switch (n) {
 case 'января':
-process.stdout.write(31-d+334);
+k=31-d+334;
 break;
 case 'февраля':
-process.stdout.write(28-d+306);
+k=28-d+306;
 break;
 case 'марта':
-process.stdout.write(31-d+275);
+k=31-d+275;
 break;
 case 'апреля':
-process.stdout.write(30-d+245);
+k=30-d+245;
 break;
 case 'мая':
-process.stdout.write(31-d+214);
+k=31-d+214;
 break;
 case 'июня':
-process.stdout.write(30-d+184);
+k=30-d+184;
 break;
-case 'июля':
-process.stdout.write(31-d+153);
+case '7':
+k=31-d+153;
 break;
 case 'августа':
-process.stdout.write(31-d+122);
+k=31-d+122;
 break;
 case 'сентября':
-process.stdout.write(30-d+92);
+k=30-d+92;
 break;
 case 'октября':
-process.stdout.write(31-d+61);
+k=31-d+61;
 break;
 case 'ноября':
-process.stdout.write(30-d+31);
+k=30-d+31;
 break;
 case 'декабря':
-process.stdout.write(31-d);
+k=31-d;
 break;
 } 
 
 }
+process.stdout.write(k);
